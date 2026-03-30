a841866f25e1ee638628952a3fef32685e828322
67160412 <67160412@gp.buu.ac.th>
2026-03-31 04:15:31 +0700

    Feat Challenge Lv2 : add Avatar color

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,6 +5,13 @@ function UserCard({ name, email }) {
     .map((n) => n[0])
     .join("");
 
+  // Logic สุ่มสี: แปลงตัวอักษรแรกเป็นรหัสตัวเลข แล้วหารเอาเศษด้วย 3
+  const charCode = name.charCodeAt(0);
+  const colorIndex = charCode % 3;
+
+  // กำหนดชุดสี: 0=น้ำเงิน, 1=เขียว, 2=ม่วง
+  const colors = ["#1e40af", "#047857", "#6b21a8"];
+  const avatarColor = colors[colorIndex];
   return (
     <div
       style={{
@@ -22,7 +29,7 @@ function UserCard({ name, email }) {
         style={{
           width: "40px",
           height: "40px",
-          background: "#1e40af",
+          background: avatarColor, // <--- เปลี่ยนตรงนี้
           color: "white",
           borderRadius: "50%",
           display: "flex",

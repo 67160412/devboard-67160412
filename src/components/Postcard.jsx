9071d570b07a3f80ed8cbae3918dba2ca99cdea5
67160412 <67160412@gp.buu.ac.th>
2026-03-31 03:37:49 +0700

    Finish: Activity 1-5

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -5,8 +5,8 @@ function PostCard({ title, body }) {
         border: "1px solid #e2e8f0",
         borderRadius: "8px",
         padding: "1rem",
-        marginBottom: "1rem",
         background: "white",
+        marginBottom: "1rem",
       }}
     >
       <h3 style={{ margin: "0 0 0.5rem", color: "#1e40af" }}>{title}</h3>
